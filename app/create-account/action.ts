417aafa1f62f5ce089e9f69f31af48e8f21ed2a3
chs <chspower1@gmail.com>
2024-04-10 22:35:07 +0900

    feat : refactoring register action

--- app/create-account/action.ts
+++ app/create-account/action.ts
@@ -7,87 +7,74 @@ import bcrypt from "bcrypt";
 import { getIronSession } from "iron-session";
 import { cookies } from "next/headers";
 import { getSession, saveSession } from "@/lib/session";
-import { checkUniqueEmail, hashedPassword } from "@/lib/auth";
+import { isEmailUnique, isUsernameUnique, createUser } from "@/lib/auth";
 
 const registerSchema = z
   .object({
     username: z.string().trim().toLowerCase().min(3, "Username must be at least 3 characters"),
-    email: z
-      .string()
-      .trim()
-      .toLowerCase()
-      .email("Invalid email")
-      .refine(checkUniqueEmail, "This email is duplicated!"),
+    email: z.string().trim().toLowerCase().email("Invalid email"),
     password: z.string(),
     // .min(8, "Password must be at least 8 characters"),
     passwordConfirm: z.string(),
   })
+  // check password is the same
   .refine((data) => data.password === data.passwordConfirm, {
     message: "Passwords do not match",
     path: ["passwordConfirm"],
+  })
+  // check usernaem is unique
+  .superRefine(async ({ username }, ctx) => {
+    if (!(await isUsernameUnique(username))) {
+      ctx.addIssue({
+        code: "custom",
+        message: "This username is already in used",
+        fatal: true,
+        path: ["username"],
+      });
+      return z.NEVER;
+    }
+  })
+  // check usernaem is email
+  .superRefine(async ({ email }, ctx) => {
+    if (!(await isEmailUnique(email))) {
+      ctx.addIssue({
+        code: "custom",
+        message: "This Eamil is already in used",
+        fatal: true,
+        path: ["email"],
+      });
+      return z.NEVER;
+    }
   });
 
 export async function registerAction(prevState: any, formData: FormData) {
   await new Promise((resolve) => setTimeout(resolve, 1000));
-
-  const data = {
-    username: formData.get("username"),
-    email: formData.get("email"),
-    password: formData.get("password"),
-    passwordConfirm: formData.get("password-confirm"),
-  };
-
-  const result = await registerSchema.safeParseAsync(data);
-
-  // guard
-  if (!result.success) {
-    console.log(result.error.flatten());
-    return result.error.flatten();
-  }
-
-  const { email, password, username: name } = result.data;
   try {
+    const data = {
+      username: formData.get("username"),
+      email: formData.get("email"),
+      password: formData.get("password"),
+      passwordConfirm: formData.get("password-confirm"),
+    };
+
+    const result = await registerSchema.safeParseAsync(data);
+
+    // guard
+    if (!result.success) {
+      console.log(result.error.flatten());
+      return result.error.flatten();
+    }
+
     // create user
-    const user = await db.user.create({
-      data: {
-        name,
-        email,
-        password: await hashedPassword(password),
-      },
-      select: {
-        id: true,
-      },
-    });
+    const user = await createUser(result.data);
 
     // iron-session
     const session = await getSession();
     saveSession(session, user.id);
-    redirect("/profile", RedirectType.push);
+
+    // redirect
   } catch (err) {
     console.log(err);
   }
-  redirect(`/`);
+  redirect("/profile", RedirectType.push);
 }
-
-const createUser = async (data: {
-  username: string;
-  email: string;
-  password: string;
-  passwordConfirm: string;
-}) => {
-  const { email, password, username: name } = data;
-  const user = await db.user.create({
-    data: {
-      name,
-      email,
-      password: await bcrypt.hash(password, 10),
-    },
-    select: {
-      id: true,
-    },
-  });
-  getIronSession(cookies(), {
-    cookieName: "iron-session",
-    password: process.env.SECRET_KEY!,
-  });
-};
